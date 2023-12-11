8b7624b284fcf0ff3d11eec6dc9a68d59bfe1c60
MAAMAR BENHADJAR <biboustre@hotmail.com>
2023-12-11 09:36:43 +0100

    Mise à jour HEADER responsive

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,21 +1,40 @@
+// import { useState, useEffect } from "react";
 import styles from "./styles/header.module.css";
 import { Link } from "react-router-dom";
 
 function Header() {
+  // const [checkWidth, setCheckWidth] = useState(window.innerWidth);
+
   return (
     <>
+          {/* {checkWidth < 600 && (
+            <nav>
+            <Link to="/" className={styles.liens_globale}>
+              <p>Accueil</p> <p className={styles.liensHidden}>ACCUEIL</p>
+            </Link>
+            <Link to="/A Propos" className={styles.liens_globale}>
+               <p>A Propos</p> <p className={styles.liensHidden}>A PROPOS</p>
+            </Link>
+          </nav>
+          )} */}
       <header>
         <div>
           <img src="images/LOGO.png" alt="" />
         </div>
+        
         <nav>
           <Link to="/" className={styles.liens_globale}>
-            Accueil
+            <p>Accueil</p> 
           </Link>
           <Link to="/A Propos" className={styles.liens_globale}>
-            A Propos
+             <p>A Propos</p> 
           </Link>
-          {/* <Link to="/orange" className={styles.liens_globale} >orange</Link> */}
+          <Link to="/" className={styles.liensHidden}>
+             <p >ACCUEIL</p>
+          </Link>
+          <Link to="/A Propos" className={styles.liensHidden}>
+              <p >A PROPOS</p>
+          </Link>
         </nav>
       </header>
     </>
