148c6f62da165f435147ab214b995d596e8b8a5e
MAAMAR BENHADJAR <biboustre@hotmail.com>
2024-02-15 23:24:04 +0100

    Modif OK

--- src/components/Molecules/Navigation/Navigation.jsx
+++ src/components/Molecules/Navigation/Navigation.jsx
@@ -1,14 +1,12 @@
 import NavLink from "../../Atoms/NavLink/NavLink";
-import { useState } from "react";
 
 const Navigation = () => {
-
-    return (
-        <nav>
-          <NavLink link="/" title="Accueil" />
-          <NavLink link="/a-propos" title="A propos" />
-        </nav>
-    )
-}
+  return (
+    <nav>
+      <NavLink link="/" title="Accueil" />
+      <NavLink link="/a-propos" title="A propos" />
+    </nav>
+  );
+};
 
 export default Navigation;
